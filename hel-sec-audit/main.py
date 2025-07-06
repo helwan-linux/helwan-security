2a97b6832159e8ab70704b4dba8bf731b1b36948
helwan-linux <152768713+helwan-linux@users.noreply.github.com>
2025-07-06 21:38:20 +0300

    Update main.py

--- hel-sec-audit/main.py
+++ hel-sec-audit/main.py
@@ -1,16 +1,16 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtGui import QIcon
+
 from gui.main_window import MainWindow
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-
-    icon = QIcon("/usr/share/pixmaps/hel-sec-audit.png")
-    app.setWindowIcon(icon)
+    
+    # تعيين أيقونة التطبيق العامة (ستظهر في شريط المهام وعنوان النافذة)
+    # تأكد من أن المسار "gui/assets/app_icon.png" صحيح وأن الملف موجود.
+    app.setWindowIcon(QIcon("gui/assets/app_icon.png")) 
 
     main_win = MainWindow()
-    main_win.setWindowIcon(icon)
     main_win.show()
-
     sys.exit(app.exec_())
